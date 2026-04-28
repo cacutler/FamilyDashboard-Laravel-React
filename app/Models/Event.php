0066a9f5d69c0e396df133adc099506c58092f0f
Alex Cutler <calexcutler@gmail.com>
2026-04-27 19:02:56 -0600

    Added a protected casts function to the Event data model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 #[Fillable(['name', 'location', 'start_date', 'end_date', 'start_time', 'end_time', 'description'])]
 class Event extends Model {
     use HasFactory;
+    protected function casts(): array {
+        return [
+            //'start_time' => 'datetime:H:i:s', //For Carbon support
+            //'end_time'   => 'datetime:H:i:s', //For Carbon support
+            'start_date' => 'date',
+            'end_date' => 'date'
+        ];
+    }
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }

95f13cd3385b1ea329e9ae33bed9551ca56a341c
Alex Cutler <calexcutler@gmail.com>
2026-04-28 09:09:48 -0600

    Added another migration file to allow for two parents to be assigned to a child account

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -25,9 +26,63 @@ class User extends Authenticatable {
         ];
     }
     public function todos(): HasMany {
-        return $this->hasMany(ToDo::class);
+        return $this->hasMany(ToDo::class, 'created_by');
+    }
+    public function assignedTodos(): HasMany {
+        return $this->hasMany(ToDo::class, 'assigned_to');
     }
     public function events(): HasMany {
         return $this->hasMany(Event::class);
+    }
+    public function parents(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'parent_id', 'child_id', 'parent_id');
+    }
+    public function children(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'parent_id', 'parent_id', 'child_id');
+    }
+    public function isParent(): bool {
+        return $this->status === 'parent';
+    }
+    public function isChild(): bool {
+        return $this->status === 'child';
+    }
+    /**
+     * Returns true if $user is in the same family as this user.
+     * Covers: self, own children, own parents, and siblings (shared parent).
+     */
+    public function isFamilyMember(User $user): bool {
+        if ($this->id === $user->id) {
+            return true;
+        }
+        if ($this->children->contains('id', $user->id)) {// Parents checking one of their children
+            return true;
+        }
+        if ($this->parents->contains('id', $user->id)) {
+            return true;
+        }
+        $myParentIds    = $this->parents->pluck('id');// Siblings share at least one common parent
+        $theirParentIds = $user->parents->pluck('id');
+        return $myParentIds->intersect($theirParentIds)->isNotEmpty();
+    }
+    /**
+     * Returns the IDs of all family members visible to this user.
+     * Parents see themselves + all their children.
+     * Children see themselves + their parents + siblings.
+     */
+    public function familyMemberIds(): array {
+        $ids = collect([$this->id]);
+        if ($this->isParent()) {
+            $ids = $ids->merge($this->children->pluck('id'));
+        } else {
+            $parentIds = $this->parents->pluck('id');
+            $ids = $ids->merge($parentIds);
+            if ($parentIds->isNotEmpty()) {
+                $siblingIds = User::query()->whereHas('parents', function ($q) use ($parentIds) {// Siblings: other children of any of this user's parents
+                    $q->whereIn('parent_user.parent_id', $parentIds);
+                })->where('id', '!=', $this->id)->pluck('id');
+                $ids = $ids->merge($siblingIds);
+            }
+        }
+        return $ids->unique()->all();
     }
 }

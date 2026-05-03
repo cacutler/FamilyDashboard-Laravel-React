f81e8994b0848566a77e658ec2f01c647ef209df
Alex Cutler <calexcutler@gmail.com>
2026-05-03 08:47:02 -0600

    Updated code styling

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,9 +5,7 @@ import { initializeTheme } from '@/hooks/use-appearance';
 import AppLayout from '@/layouts/app-layout';
 import AuthLayout from '@/layouts/auth-layout';
 import SettingsLayout from '@/layouts/settings/layout';
-
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
@@ -35,6 +33,5 @@ createInertiaApp({
         color: '#4B5563',
     },
 });
-
 // This will set light / dark mode on load...
 initializeTheme();
